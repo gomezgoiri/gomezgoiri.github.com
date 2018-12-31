7e096da0736ea492482144e9261c3387ca763dfb
Aitor Gomez Goiri <aitor@gomezgoiri.net>
2018-12-31 13:57:53 +0100

    Adding boilerplate for new React personal page

---
+++ gh-react/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+    </header>
+    <p>New personal page version.</p>
+  </div>
+)
+
+export default App
