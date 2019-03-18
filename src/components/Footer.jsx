13d1ff023ca6348aa73b90734aa4a206fca4eea4
Aitor Gomez Goiri <aitor@gomezgoiri.net>
2019-03-18 19:46:30 +0100

    Fixing spacing issues in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,8 +11,8 @@ const Footer = () => (
           <a href="https://github.com/gomezgoiri/gomezgoiri.github.com">
             GitHub Pages
           </a>
-          , theme by
-          <a href="http://getbootstrap.com">Bootstrap</a>, based on{' '}
+          , using <a href="http://getbootstrap.com">Bootstrap</a> theme and
+          based on{' '}
           <a href="https://github.com/tscanlin/next-blog">
             Tim Scanlin&apos;s next-blog
           </a>
